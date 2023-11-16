3fd5e9f9b962fb60b9bfe4f93113e817f8bb9ce8
Oleksii Kaunenko <kaunenko@gmail.com>
2023-11-17 01:27:04 +0200

    Add a new script

---
+++ sys-load-monitor.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Keep track of your server’s system load and resource usage with this simple shell script:
+echo "System Load: $(uptime)"
+echo "Free Memory: $(free -h | grep Mem | awk '{print $4}')"
+echo "Free Disk Space: $(df -h / | grep / | awk '{print $4}')"
