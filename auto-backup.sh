f2b3ecbaa509c767a4fdd386e79062c62b5febf0
Oleksii Kaunenko <kaunenko@gmail.com>
2023-11-17 19:12:16 +0200

    Added sourse and distanation and time stamp.

--- auto-backup.sh
+++ auto-backup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 # Create backups of your critical files and directories with this script, which appends a timestamp to the backup file:
-
-
+# --------------------------
+SOURCE="/home/loki/Projects/"
+DESTINATION="/home/loki/backup"
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+tar -czf "${DESTINATION}backup_${TIMESTAMP}.tar.gz" --absolute-names "${SOURCE}"
