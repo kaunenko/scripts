1bcaa1cebfcd2b52bb2c2f159ed95721b2906768
Oleksii Kaunenko <kaunenko@gmail.com>
2023-11-20 17:37:13 +0200

    Added description

--- log-file-rotation.sh
+++ log-file-rotation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Usage: ./user_management.sh [create|delete|update] username
-
+Prevent log files from growing too large by automating log file rotation with this script:
 ACTION=$1
 USERNAME=$2
 
