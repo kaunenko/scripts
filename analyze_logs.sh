cbc0f355ba04e2b0dafc13addf20cf2f46e01db7
Oleksii Kaunenko <kaunenko@gmail.com>
2025-07-25 10:41:18 +0300

    Added new script for analithes log on web server

---
+++ analyze_logs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+LOG_FILE="access.log"
+
+echo " Top 10 ip:"
+awk '{print $1}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -10
+
+echo -e "\n Status HTTP:"
+awk '{print $9}' "$LOG_FILE" | sort | uniq -c | sort -nr
+
+echo -e "\n Top 10 URL-requests:"
+awk -F\" '{print $2}' "$LOG_FILE" | awk '{print $2}' | sort | uniq -c | sort -nr | head -10
