2bf3697ce5adced7dc51f6233e5338bdd8567601
Oleksii Kaunenko <kaunenko@gmail.com>
2025-07-25 09:37:22 +0300

    Dell comment

--- backup.sh
+++ backup.sh
@@ -11,4 +11,4 @@ BACKUP_FILE="$BUCKUP_DIR/etc-$DATE.tar.gz"
 tar -czf "$BACKUP_FILE" /etc
 
 #Delliting backup older 7 days
-#find "$BUCKUP_DIR" -type f -name "etc-*.tar.gz" -mtime +7 -exec rm -f {} \;
+find "$BUCKUP_DIR" -type f -name "etc-*.tar.gz" -mtime +7 -exec rm -f {} \;
